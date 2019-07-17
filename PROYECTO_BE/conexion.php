5b5339ae98b069332e1a4130999d29e1e8e6b4e4
Roberto w <roberto.wbb@hotmail.com>
2019-07-16 22:15:42 -0600

    Conexion BD

---
+++ PROYECTO_BE/conexion.php
@@ -0,0 +1,14 @@
+<DOCTYPE HTML>
+    <meta charset = "utf8"/>
+    <?php
+    //crear conexion con oracle
+    $conexion = oci_connect("hr","hr","localhost/xe");
+
+    if(!$conexion){
+        $m = oci_error();
+        echo $m('message'),"n";
+        exit;
+    }else{
+        echo "Conexion con exito a Oracle";
+    }
+    ?>
